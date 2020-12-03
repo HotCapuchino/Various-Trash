e4da55b1cec0af6b6c743f530d31f38d628e7257
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-12-03 15:32:07 +0800

    Исправил Synthesizer

--- AzureVoiceSynthesizer/VoiceSynthesizer.py
+++ AzureVoiceSynthesizer/VoiceSynthesizer.py
@@ -12,11 +12,15 @@ class Synthesizer:
         headers = {"X-Microsoft-OutputFormat": "audio-16khz-128kbitrate-mono-mp3",
                    "Content-Type": "application/ssml+xml",
                    "User Agent": "My_app",
-                   "Bearer": token}
+                   "Authorization": "Bearer " + token}
         body = "<speak version='1.0' xml:lang='" + self.__user_data["lang"] + \
-               '><voice xml:lang=' + self.__user_data["lang"] + \
-               ' xml:gender=' + self.__user_data["gender"] + \
-               ' name=' + self.__user_data["name"] + "\'>" + self.__user_data["text"] + "</voice></speak>"
-        request = requests.post(self.__auth_data.getHost(), headers=headers, data=body.encode('utf-8'))
-        response = request.status_code
-        return response
+               "'><voice xml:lang='" + self.__user_data["lang"] + \
+               "' xml:gender='" + self.__user_data["gender"] + \
+               "' name='" + self.__user_data["name"] + "\'>" + self.__user_data["text"] + "</voice></speak>"
+        request = requests.post(self.__auth_data.getHost(), headers=headers, data=body)
+        response = request.content
+        if request.status_code != 200:
+            print("Oops! Error occurred while synthesizing the file!")
+            return -1
+        else:
+            return response
