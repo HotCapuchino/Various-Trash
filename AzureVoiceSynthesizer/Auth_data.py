cde427c4a9e7dadcef90ef9eaf77c76c717cff16
HotCapuchino <36916169+HotCapuchino@users.noreply.github.com>
2020-12-14 14:41:46 +0800

    Update Auth_data.py

--- AzureVoiceSynthesizer/Auth_data.py
+++ AzureVoiceSynthesizer/Auth_data.py
@@ -1,7 +1,7 @@
 
 class AuthData:
 
-    __key = "1db01788ad86488d90d573a7fe502c11"
+    __key = "Your subscription key here"
     __auth_url = "https://eastasia.api.cognitive.microsoft.com/sts/v1.0/issueToken"
     __lang_url = "https://eastasia.tts.speech.microsoft.com/cognitiveservices/voices/list"
     __host = "https://eastasia.tts.speech.microsoft.com/cognitiveservices/v1"
